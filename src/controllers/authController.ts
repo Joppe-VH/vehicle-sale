bc5c48117fba5323f12b6162809eee6b9101d167
Joppe-VH <joppe.vanhoof@gmail.com>
2025-02-10 11:13:42 +0100

    add register route

---
+++ src/controllers/authController.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { Error as MongooseError } from "mongoose";
+const { ValidationError } = MongooseError;
+import { User } from "../models/userModel";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import e from "cors";
+import { JWT_SECRET, NODE_ENV } from "../config/env";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(403).json({ message: "Email already in use" });
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    if (!JWT_SECRET) throw new Error("no JWT secret available");
+    const token = jwt.sign(
+      { _id: newUser._id, email: newUser.email },
+      JWT_SECRET as string
+    );
+    res.cookie("token", token, {
+      maxAge: 24 * 60 * 60 * 1000 /* 1 dag */,
+      httpOnly: true,
+      secure: NODE_ENV === "production",
+      sameSite: "lax",
+    });
+    res.status(201).json({ status: "success", data: newUser });
+  } catch (error: unknown) {
+    if (error instanceof ValidationError) {
+      res.status(400).json({ message: error.message });
+    } else if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+};
