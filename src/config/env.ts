c3b4a74098599fb291608567e58a008c02c169bb
Joppe-VH <joppe.vanhoof@gmail.com>
2025-02-12 15:24:32 +0100

    add arcjet

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,8 @@ export const PORT = process.env.PORT || 3000;
 export const MONGO_URI = process.env.MONGO_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const NODE_ENV = process.env.NODE_ENV || "development";
+export const ARCJET_KEY = process.env.ARCJET_KEY as string;
 
-if (!MONGO_URI || !JWT_SECRET) {
+if (!MONGO_URI || !JWT_SECRET || !ARCJET_KEY) {
   throw new Error("Missing environment variables");
 }
